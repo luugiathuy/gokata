147d28b4b5f232ae4a8adc90bcaffd0714e6acde
Luu Gia Thuy <luugiathuy@gmail.com>
2015-04-13 14:17:19 +0800

    Use WordSpec instead of FlatSpec

--- src/test/scala/GokataSpec.scala
+++ src/test/scala/GokataSpec.scala
@@ -1,20 +1,8 @@
 import org.scalatest._
 
-class GokataSpec extends FlatSpec {
+class GokataSpec extends WordSpec {
 
   val newGoGame = new NewGoGame(5, 5)
-
-  "The move at the start of a game" must "be black" in {
-    assert(newGoGame.isLegalMove(Move(0, 0, BlackPiece)))
-    assert(!newGoGame.isLegalMove(Move(0, 0, WhitePiece)))
-  }
-
-  "The move's position" must "be inside the game board" in {
-    assert(!newGoGame.isLegalMove(Move(-1, 2, BlackPiece)))
-    assert(!newGoGame.isLegalMove(Move(3, -1, BlackPiece)))
-    assert(!newGoGame.isLegalMove(Move(5, 1, BlackPiece)))
-    assert(!newGoGame.isLegalMove(Move(4, 5, BlackPiece)))
-  }
 
   trait SelfCaptureGame1 extends StringParserGoGame {
     val board =
@@ -40,23 +28,8 @@ class GokataSpec extends FlatSpec {
       """.stripMargin
     addBoardToHistory
   }
-  
-  "The move" must "not be at occupied position" in {
-    new SelfCaptureGame1 {
-      assert(!isLegalMove(Move(0, 1, WhitePiece)))
-    }
-  }
 
-  "The move" must "not be placed where it would be immediately captured" in {
-    new SelfCaptureGame1 {
-      assert(!isLegalMove(Move(0, 0, WhitePiece)))
-    }
-    new SelfCaptureGame2 {
-      assert(!isLegalMove(Move(1, 1, WhitePiece)))
-    }
-  }
-
-  trait NonSelfCaptureGame extends StringParserGoGame {
+  trait KoRuleGame extends StringParserGoGame {
     val board =
       """o
         |-----
@@ -68,9 +41,56 @@ class GokataSpec extends FlatSpec {
     addBoardToHistory
   }
 
-  "The move" can "be placed where it captures enemy's positions, even its position has no liberty" in {
-    new NonSelfCaptureGame {
-      assert(isLegalMove(Move(2, 1, WhitePiece)))
+  "isLegalMove()" when {
+    "at the start of the game" should {
+      "be true if black move" in {
+        assert(newGoGame.isLegalMove(Move(0, 0, BlackPiece)))
+      }
+
+      "be false if white move" in {
+        assert(!newGoGame.isLegalMove(Move(0, 0, WhitePiece)))
+      }
+    }
+
+    "the move is outside the board's coordinate" must {
+      "be false" in {
+        assert(!newGoGame.isLegalMove(Move(-1, 2, BlackPiece)))
+        assert(!newGoGame.isLegalMove(Move(3, -1, BlackPiece)))
+        assert(!newGoGame.isLegalMove(Move(5, 1, BlackPiece)))
+        assert(!newGoGame.isLegalMove(Move(4, 5, BlackPiece)))
+      }
+    }
+
+    "the move is on occupied position" must {
+      "be false" in {
+        new SelfCaptureGame1 {
+          assert(!isLegalMove(Move(0, 1, WhitePiece)))
+        }
+      }
+    }
+
+    "the move has no liberties (self-capturing)" must {
+      "be false" in {
+        new SelfCaptureGame1 {
+          assert(!isLegalMove(Move(0, 0, WhitePiece)))
+        }
+      }
+    }
+
+    "the move causes its connected group has no liberties (self-capturing)" must{
+      "be false" in {
+        new SelfCaptureGame2 {
+          assert(!isLegalMove(Move(1, 1, WhitePiece)))
+        }
+      }
+    }
+
+    "the move captures enemy's positions, even its position has no liberties" should {
+      "be true" in {
+        new KoRuleGame {
+          assert(isLegalMove(Move(2, 1, WhitePiece)))
+        }
+      }
     }
   }
 }
